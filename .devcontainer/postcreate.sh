dfaee4379f2ee6fa9f18db81a45c53c847bfe7cd
Joshua Rich <joshua.rich@gmail.com>
2024-10-30 10:14:17 +1000

    build(devcontainer): :wrench: clean up postcreate script

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -5,7 +5,7 @@ set -x
 # Install starship prompt.
 cd /tmp && curl -sS https://starship.rs/install.sh | sh -s -- -y || exit -1
 mkdir -p ~/.config/fish
-echo "starship init fish | source" >> ~/.config/fish/config.fish
+# echo "starship init fish | source" >> ~/.config/fish/config.fish
 
 # Install parceljs.
 npm install --save-dev parcel || exit -1
@@ -14,7 +14,5 @@ npm install --save-dev parcel || exit -1
 go install github.com/a-h/templ/cmd/templ@latest
 # Install latest air command.
 go install github.com/air-verse/air@latest
-# Install latest stringer command.
-go install golang.org/x/tools/cmd/stringer@latest
 
 exit 0
