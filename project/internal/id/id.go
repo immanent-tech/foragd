b850a6597cd2b7721f69b79c770d29de4284cf27
Joshua Rich <joshua.rich@gmail.com>
2025-01-20 14:23:46 +1000

    feat(internal/id): :sparkles: add a method to identify the type of ID from an ID string

--- project/internal/id/id.go
+++ project/internal/id/id.go
@@ -1,31 +1,48 @@
 // Copyright 2024 Joshua Rich <joshua.rich@gmail.com>.
 // SPDX-License-Identifier: 	AGPL-3.0-or-later
 
-//go:generate go run golang.org/x/tools/cmd/stringer -type=Option -linecomment -output id_generated.go
+//go:generate go run golang.org/x/tools/cmd/stringer -type=Prefix -linecomment -output id_generated.go
 package id
 
 import (
 	"fmt"
+	"strings"
 
 	nanoid "github.com/matoous/go-nanoid"
 )
 
 const (
-	Sub  Option = iota // sub
-	Feed               // feed
-	Item               // item
+	Unknown      Prefix = iota // unknown
+	Subscription               // sub
+	Feed                       // feed
+	Item                       // item
 )
 
-// Option represents a type of ID. Specific types share a common prefix.
-type Option int
+// Prefix represents a type of ID. Specific types share a common prefix.
+type Prefix int
 
 // NewID generates a new unique ID for the given type option. If an ID cannot be
 // generated, a non-nil error is returned.
-func NewID(option Option) (string, error) {
+func NewID(option Prefix) (string, error) {
 	id, err := nanoid.Nanoid()
 	if err != nil {
 		return "", fmt.Errorf("could not generate username: %w", err)
 	}
 
 	return option.String() + "_" + id, nil
+}
+
+// IdentifyID takes an ID and returns the type of ID it represents.
+func IdentifyID(id string) Prefix {
+	idParts := strings.Split(id, "_")
+	switch idParts[0] {
+	case Feed.String():
+		return Feed
+	case Item.String():
+		return Item
+	case Subscription.String():
+		return Subscription
+	default:
+		return Unknown
+	}
 }
