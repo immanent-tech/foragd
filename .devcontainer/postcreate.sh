8fb7915c258a2eee314b1463c6ad88f577756a46
Joshua Rich <joshua.rich@gmail.com>
2025-02-21 13:15:15 +1000

    chore(devcontainer): :fire: clean up postcreate devcontainer script

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -10,7 +10,7 @@ apt-get update && export DEBIAN_FRONTEND=noninteractive \
 # Install starship prompt.
 cd /tmp && curl -sS https://starship.rs/install.sh | sh -s -- -y || exit -1
 mkdir -p ~/.config/fish
-# echo "starship init fish | source" >> ~/.config/fish/config.fish
+echo "starship init fish | source" >> ~/.config/fish/config.fish
 
 cd /workspace/project
 
@@ -18,10 +18,5 @@ cd /workspace/project
 npm install --save-dev parcel || exit -1
 
 go mod tidy
-
-# Install latest templ command.
-go install github.com/a-h/templ/cmd/templ@latest
-# Install latest air command.
-go install github.com/air-verse/air@latest
 
 exit 0
