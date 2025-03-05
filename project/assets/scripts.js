f4c8b79c2273d2e3fb31b9b2416739697c680dee
Joshua Rich <joshua.rich@gmail.com>
2025-03-06 08:42:25 +1000

    fix(assets/scripts): :bug: remove unused htmx extensions

--- project/assets/scripts.js
+++ project/assets/scripts.js
@@ -4,10 +4,6 @@
 // htmx
 import 'htmx.org'
 import './setup-htmx.js'
-// htmx server side events extension
-import 'htmx.org/dist/ext/sse'
-// htmx websocket extension
-import 'htmx.org/dist/ext/ws'
 // htmx morphdom-swap extension
 import 'htmx.org/dist/ext/morphdom-swap'
 
