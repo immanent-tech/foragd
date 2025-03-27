dca730aeb45b70f544c6c68500307f2dddd6e8d6
Joshua Rich <joshua.rich@gmail.com>
2025-03-28 09:11:00 +1000

    feat(pkg/feeds): :sparkles: rss schema with dublin core extensions

---
+++ project/pkg/feeds/generate.go
@@ -0,0 +1,8 @@
+// Copyright 2024 Joshua Rich <joshua.rich@gmail.com>.
+// SPDX-License-Identifier: 	AGPL-3.0-or-later
+
+package feeds
+
+//go:generate go tool github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config rss-cfg.yaml rss.yaml
+//go:generate go tool github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config rdf-cfg.yaml rdf.yaml
+//go:generate go tool github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config atom-cfg.yaml atom.yaml
---
+++ project/pkg/feeds/rss/rss.go
@@ -0,0 +1,27 @@
+// Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
+// SPDX-License-Identifier: 	AGPL-3.0-or-later
+
+package rss
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+
+	"golang.org/x/net/html/charset"
+)
+
+// New generates an RSS object from the given byte array.
+func New(b []byte) (*RSS, error) {
+	var feed RSS
+
+	reader := bytes.NewReader(b)
+	decoder := xml.NewDecoder(reader)
+	decoder.CharsetReader = charset.NewReaderLabel
+	err := decoder.Decode(&feed)
+	if err != nil {
+		return nil, fmt.Errorf("could not decode byte array to RSS: %w", err)
+	}
+
+	return &feed, nil
+}
