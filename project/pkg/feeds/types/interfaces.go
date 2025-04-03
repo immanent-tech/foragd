12d0b5146c7e92d2dcb50bf374fc69a8f980b18b
Joshua Rich <joshua.rich@gmail.com>
2025-04-03 14:40:12 +1000

    refactor(pkg/feeds): :truck: restructure types package

---
+++ project/pkg/feeds/types/interfaces.go
@@ -0,0 +1,78 @@
+// Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
+// SPDX-License-Identifier: 	AGPL-3.0-or-later
+
+package types
+
+// ObjectID contains methods for retrieving an Objects unique ID.
+type ObjectID interface {
+	GetID() string
+}
+
+// ObjectMedia contains methods for retrieving an Object's media, such as audio and video.
+type ObjectMedia interface {
+	GetImage() *Image
+}
+
+// ObjectMetadata contains methods for retrieving the metadata information about the Object.
+type ObjectMetadata interface {
+	GetTitle() string
+	GetDescription() string
+	GetLink() string
+	GetPublishedDate() DateTime
+	GetUpdatedDate() DateTime
+}
+
+// ObjectAttribution contains methods for retrieving values that relate to the copyright, rights, authors and
+// contributors of an Object.
+type ObjectAttribution interface {
+	GetAuthors() []string
+	GetContributors() []string
+}
+
+// ObjectContent contains methods for retrieving any embedded content of the Object.
+type ObjectContent interface {
+	GetContent() *Content
+}
+
+// ObjectTaxonomy contains methods for retrieving categorization and taxonomy values of an Object.
+type ObjectTaxonomy interface {
+	GetCategories() []*Category
+}
+
+// ObjectSource contains methods for retrieving or setting the source of the Object.
+type ObjectSource interface {
+	GetSourceURL() string
+	SetSourceURL(url string)
+}
+
+// ItemSource is an abstraction representing an individual Item from any type of Feed source.
+type ItemSource interface {
+	ObjectMetadata
+	ObjectAttribution
+	ObjectContent
+	ObjectTaxonomy
+	ObjectID
+	ObjectMedia
+}
+
+// Item represents a single item or entry (or article) in a feed.
+type Item struct {
+	ItemSource
+}
+
+type Items []Item
+
+// FeedSource is an abstraction representing any type of Feed.
+type FeedSource interface {
+	ObjectMetadata
+	ObjectSource
+	ObjectAttribution
+	ObjectTaxonomy
+	ObjectMedia
+	GetItems() []Item
+}
+
+// Feed represents any feed type containing a number of items.
+type Feed struct {
+	FeedSource
+}
--- project/pkg/feeds/types/types.go
+++ project/pkg/feeds/types/types.go
@@ -15,68 +15,6 @@ var MimeTypesRSS = []string{"application/rss+xml", "application/rdf+xml"}
 var MimeTypesAtom = []string{"application/atom+xml"}
 var MimeTypesIndeterminate = []string{"application/xml", "text/xml"}
 
-type ObjectID interface {
-	GetID() string
-}
-
-type ObjectMedia interface {
-	GetImage() *Image
-}
-
-type ObjectMetadata interface {
-	GetTitle() string
-	GetDescription() string
-	GetLink() string
-	GetPublishedDate() DateTime
-	GetUpdatedDate() DateTime
-}
-
-type ObjectAttribution interface {
-	GetAuthors() []string
-	GetContributors() []string
-}
-
-type ObjectContent interface {
-	GetContent() *Content
-}
-
-type ObjectTaxonomy interface {
-	GetCategories() []*Category
-}
-
-type ObjectSource interface {
-	GetSourceURL() string
-	SetSourceURL(url string)
-}
-
-type ItemSource interface {
-	ObjectMetadata
-	ObjectAttribution
-	ObjectContent
-	ObjectTaxonomy
-	ObjectID
-	ObjectMedia
-}
-
-// Item represents a single item or entry (or article) in a feed.
-type Item struct {
-	ItemSource
-}
-
-type FeedSource interface {
-	ObjectMetadata
-	ObjectSource
-	ObjectAttribution
-	ObjectTaxonomy
-	ObjectMedia
-	GetItems() []Item
-}
-
-// Feed represents any feed type containing a number of items.
-type Feed struct {
-	FeedSource
-}
-
 // String will return the value of the object.
 func (c *CustomTypeBase) String() string {
 	return c.Value
