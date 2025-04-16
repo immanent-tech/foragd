1b60ddde3ec441c8e51b0bcbf648d01ded4b22c6
Joshua Rich <joshua.rich@gmail.com>
2025-04-17 08:23:21 +1000

    build(devcontainer): :heavy_plus_sign: install graphviz for go profiling

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -4,7 +4,7 @@ set -x
 
 # Install additional packages.
 sudo apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && sudo apt-get -y install --no-install-recommends micro pre-commit \
+    && sudo apt-get -y install --no-install-recommends micro pre-commit graphviz \
     && sudo apt-get -y autoremove && sudo apt-get -y clean && sudo rm -rf /var/lib/apt/lists/*
 
 # Install step cli.
