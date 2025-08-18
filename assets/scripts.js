a2d7c217927958938ba0887cca78ab27d9ce54ac
Joshua Rich <joshua.rich@gmail.com>
2025-08-19 09:08:17 +1000

    feat: :sparkles: use preload htmx extension to preload next page when viewing subscription/article cards

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,11 +5,13 @@
 import 'htmx.org'
 import './htmx.js'
 import 'htmx-ext-response-targets'
-import 'htmx-ext-multi-swap'
+import 'htmx-ext-sse'
 import 'htmx-ext-ajax-header'
+import 'htmx-ext-preload'
 // hyperscript
 import _hyperscript from 'hyperscript.org/dist/_hyperscript.js'
 _hyperscript.browserInit()
+// custom element for timestamps
 import './timestamps.js'
 // CSS anchor positioning polyfill for browsers that don't support it natively.
 // https://caniuse.com/css-anchor-positioning
