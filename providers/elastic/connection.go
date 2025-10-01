e9c309e7feff8b32581195b64bd44e9993252ec1
Joshua Rich <joshua.rich@gmail.com>
2025-10-01 04:19:17 +0000

    perf(elastic): :zap: adjust connection settings
    - increase timeouts
    - improve tls security

--- providers/elastic/connection.go
+++ providers/elastic/connection.go
@@ -14,17 +14,31 @@ import (
 	elasticsearch "github.com/elastic/go-elasticsearch/v9"
 )
 
-const (
-	maxIdleConnsPerHost = 10
-	connTimeout         = time.Second
-)
-
 var defaultTransportConfig = &http.Transport{
-	MaxIdleConnsPerHost:   maxIdleConnsPerHost,
-	ResponseHeaderTimeout: connTimeout,
-	DialContext:           (&net.Dialer{Timeout: connTimeout}).DialContext,
+	ResponseHeaderTimeout: 5 * time.Second,
+	IdleConnTimeout:       120 * time.Second,
+	MaxIdleConnsPerHost:   10,
+	DialContext:           (&net.Dialer{Timeout: 10 * time.Second}).DialContext,
 	TLSClientConfig: &tls.Config{
+		// Only use curves which have assembly implementations
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519, // Go 1.8 only
+		},
 		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+
+			// Best disabled, as they don't provide Forward Secrecy,
+			// but might be necessary for some clients
+			// tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			// tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+		},
 	},
 }
 
