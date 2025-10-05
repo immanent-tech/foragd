1524ffe8027bd34fc662a012b1c58fe99ab7ee81
Joshua Rich <joshua.rich@gmail.com>
2025-10-05 22:35:42 +0000

    fix(layouts): :lipstick: fix size of logo on landing page

--- web/templates/layouts/landing_templ.go
+++ web/templates/layouts/landing_templ.go
@@ -85,7 +85,7 @@ func Landing() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = partials.Logo("logo-landing").Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = partials.Logo("logo-landing", "md:mx-auto", "md:size-96").Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
