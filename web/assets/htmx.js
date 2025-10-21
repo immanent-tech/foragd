4c26baa9c15c82947ecec9b8e1e69bf88e6ee4e7
Joshua Rich <joshua.rich@gmail.com>
2025-10-21 23:32:54 +0000

    build: :heavy_plus_sign: add and adjust frontend dependencies
    - add surreal
    - add css positioning polyfill

--- web/assets/htmx.js
+++ web/assets/htmx.js
@@ -1,5 +1,5 @@
 // Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
 // SPDX-License-Identifier: 	AGPL-3.0-or-later
 
-import htmx from "htmx.org";
+import htmx from 'htmx.org'
 window.htmx = htmx
