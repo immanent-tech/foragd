716fb018a61c3f5c1f28b7a8391ef812e76c07e1
Joshua Rich <joshua.rich@gmail.com>
2025-10-29 01:50:44 +0000

    refactor(cli): :recycle: adjust to allow multiple scheduler cli commands

--- cli/scheduler.go
+++ cli/scheduler.go
@@ -16,10 +16,15 @@ import (
 )
 
 // SchedulerCmd defines the `scheduler` command, for running the job scheduler.
-type SchedulerCmd struct{}
+type SchedulerCmd struct {
+	Run RunSchedulerCmd `cmd:"delete" help:"Run scheduler."`
+	// RemoveOrphanFeedJobs RemoveOrphanFeedJobs `cmd:"remove-orphan-feed-jobs" help:"Remove feed jobs that have no corresponding feed.`
+}
+
+type RunSchedulerCmd struct{}
 
 // Run contains logic for setup and execution of the scheduler.
-func (r *SchedulerCmd) Run(opts *Arguments) error {
+func (r *RunSchedulerCmd) Run(opts *Arguments) error {
 	// Set up context.
 	ctx, cancelFunc := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancelFunc()
@@ -31,3 +36,20 @@ func (r *SchedulerCmd) Run(opts *Arguments) error {
 	}
 	return nil
 }
+
+// type RemoveOrphanFeedJobs struct{}
+
+// func (c *RemoveOrphanFeedJobs) Run() error {
+// 	// Set up context.
+// 	ctx, cancelFunc := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+// 	defer cancelFunc()
+// 	ctx = elastic.SetupIndexAliases(ctx)
+// 	// Load the Elastic backend
+// 	env := os.Getenv("FORAGD_ENVIRONMENT")
+// 	client, err := elastic.Connect(ctx, env)
+// 	if err != nil {
+// 		return fmt.Errorf("failed to connect to backend: %w", err)
+// 	}
+
+// 	return nil
+// }
