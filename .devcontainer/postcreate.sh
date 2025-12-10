46ebb97f32670cf26a550073fb7ee16cfe81d96d
Joshua Rich <joshua.rich@gmail.com>
2025-12-10 11:30:29 +0000

    build(devcontainer): :coffin: devcontainer environment clean up
    - don't expose ports unnecessarily
    - remove stepca

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -19,12 +19,8 @@ go mod tidy
 go install github.com/air-verse/air@latest
 go install github.com/a-h/templ/cmd/templ@latest
 
-# Install the step CA root CA cert.
-step ca bootstrap --ca-url https://stepca:9000 --fingerprint ${CA_FINGERPRINT} --install
-
-
+# Install Stripe CLI.
 curl https://github.com/stripe/stripe-cli/releases/download/v1.33.0/stripe_1.33.0_linux_x86_64.tar.gz | tar xvf -C /tmp && \
     sudo mv stripe /usr/local/bin
-
 
 exit 0
