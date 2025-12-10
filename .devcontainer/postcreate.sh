00a7af5527bd755ac71ef1c0c940341a55d2cd24
Joshua Rich <joshua.rich@gmail.com>
2025-12-10 12:07:07 +0000

    build(devcontainer): :bug: container fixes
    - fix stripe cli install
    - fix filebeat volume

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -20,7 +20,9 @@ go install github.com/air-verse/air@latest
 go install github.com/a-h/templ/cmd/templ@latest
 
 # Install Stripe CLI.
-curl https://github.com/stripe/stripe-cli/releases/download/v1.33.0/stripe_1.33.0_linux_x86_64.tar.gz | tar xvf -C /tmp && \
-    sudo mv stripe /usr/local/bin
+cd /tmp \
+    && curl -L -O https://github.com/stripe/stripe-cli/releases/download/v1.33.0/stripe_1.33.0_linux_x86_64.tar.gz \
+    && tar xvf stripe* \
+    && sudo mv stripe /usr/local/bin
 
 exit 0
