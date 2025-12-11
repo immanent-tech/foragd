dffb775b0425862de5585b1e8210a6aa80407742
Joshua Rich <joshua.rich@gmail.com>
2025-12-11 23:44:23 +0000

    refactor(templates): :recycle: updates toast should take user back to top of page

--- web/templates/notifications_templ.go
+++ web/templates/notifications_templ.go
@@ -361,7 +361,7 @@ func UpdatesToast() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 21, "\"><span class=\"sr-only\">Refresh</span>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 21, "\" hx-swap=\"innerHTML transition:true show:window:top\"><span class=\"sr-only\">Refresh</span>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
