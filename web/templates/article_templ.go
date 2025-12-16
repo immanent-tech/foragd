a86e222b7da10f5298c31427f0f239ac6936d23b
Joshua Rich <joshua.rich@gmail.com>
2025-12-16 05:18:38 +0000

    refactor(templates): :lipstick: tighter tracking of titles on article cards

--- web/templates/article_templ.go
+++ web/templates/article_templ.go
@@ -286,7 +286,7 @@ func ArticleCard(article *models.Article) templ.Component {
 				}()
 			}
 			ctx = templ.InitializeContext(ctx)
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 16, "<h2 class=\"card-title text-pretty line-clamp-3\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 16, "<h2 class=\"card-title text-pretty line-clamp-3 tracking-tight\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
