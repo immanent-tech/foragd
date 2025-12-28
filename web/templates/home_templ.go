05eb69b2d969a84e90ebc6c35088ce6c6f0e6cc9
Joshua Rich <joshua.rich@gmail.com>
2025-12-28 23:18:31 +0000

    refactor(templates): :lipstick: make cards on home page stand out more

--- web/templates/home_templ.go
+++ web/templates/home_templ.go
@@ -102,7 +102,7 @@ func (page *Home) Template() templ.Component {
 						if templ_7745c5c3_Err != nil {
 							return templ_7745c5c3_Err
 						}
-						templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "\" class=\"card lg:card-side w-full h-full max-h-128 overflow-hidden bg-base-100 shadow-md\">")
+						templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "\" class=\"card lg:card-side w-full h-full max-h-128 overflow-hidden bg-base-200 shadow-md\">")
 						if templ_7745c5c3_Err != nil {
 							return templ_7745c5c3_Err
 						}
