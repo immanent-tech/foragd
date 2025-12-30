4b02fc6b213c53f423dc17ab1a6ca7141f42d3d0
Joshua Rich <joshua.rich@gmail.com>
2025-12-30 22:49:11 +0000

    fix(assets): :bug: make sure htmx global variable gets set correctly

--- web/assets/htmx-setup.js
+++
@@ -1,5 +0,0 @@
-window.htmx = require('htmx.org')
-
-import 'htmx-ext-preload'
-import 'htmx-ext-response-targets'
-import 'htmx-ext-sse'
--- web/assets/scripts.js
+++ web/assets/scripts.js
@@ -2,8 +2,12 @@
 // SPDX-License-Identifier: 	AGPL-3.0-or-later
 
 // htmx
-import 'htmx.org'
-import './htmx-setup'
+import htmx from 'htmx.org'
+window.htmx = htmx
+
+import 'htmx-ext-preload'
+import 'htmx-ext-response-targets'
+import 'htmx-ext-sse'
 
 // hyperscript
 import _hyperscript from 'hyperscript.org/dist/_hyperscript.js'
