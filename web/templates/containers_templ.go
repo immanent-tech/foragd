5771764a4aa9c54b57addc3143977b1fc722b402
Joshua Rich <joshua.rich@gmail.com>
2026-01-27 04:24:24 +0000

    refactor(templates): :lipstick: consistent container sizing on documents

--- web/templates/containers_templ.go
+++ web/templates/containers_templ.go
@@ -44,7 +44,7 @@ func Document(data []byte) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-8\"><article class=\"prose sm:prose-lg prose-img:shadow-md my-4\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"ui-container\"><article class=\"prose sm:prose-lg prose-img:shadow-md my-4\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
