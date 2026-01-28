afc6136abf68df96064ccecd1ac835691ce28a0a
Joshua Rich <joshua.rich@gmail.com>
2026-01-28 00:23:38 +0000

    feat(handlers): :sparkles: internal errors can now be rendered in page or as notification depending on request method

--- server/handlers/error.go
+++ server/handlers/error.go
@@ -16,49 +16,65 @@ import (
 	"github.com/immanent-tech/foragd/web/templates"
 )
 
-// Error represents any kind of error.
-type Error struct {
-	template templ.Component
-}
-
 // InternalError represents errors shown on internal (pages accessible to logged in users) pages.
-type InternalError Error
+type InternalError struct {
+	err *models.APIError
+}
 
 // HandleInternalError handles display errors on internal pages (pages accessible to logged in users).
 func HandleInternalError(err error) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
 		var apiErr *models.APIError
 		if errors.As(err, &apiErr) {
-			user, _ := models.UserFromCtx(req.Context())
 			apiErr.WriteLog(req.Context())
 			res.WriteHeader(apiErr.HTTPStatus())
 			page := &InternalError{
-				template: templates.InternalError(user, apiErr.GetUserMessage()),
+				err: apiErr,
 			}
 			RenderPage(page).ServeHTTP(res, req)
 		} else {
-			slogctx.FromCtx(req.Context()).Error("Unknown error occurred.",
-				slog.Any("error", err),
-			)
-			res.WriteHeader(http.StatusInternalServerError)
-			return
+			apiErr = &models.APIError{
+				StatusCode:    http.StatusInternalServerError,
+				InternalError: err,
+				UserMessage: models.NewErrorMessage(
+					"A backend error occurred",
+					"This might be temporary, please try again.",
+				),
+			}
+			apiErr.WriteLog(req.Context())
+			page := &InternalError{
+				err: apiErr,
+			}
+			res.WriteHeader(apiErr.HTTPStatus())
+			RenderPage(page).ServeHTTP(res, req)
 		}
 	}
 }
 
 // FullResponse renders the error message on a full page.
 func (p *InternalError) FullResponse(res http.ResponseWriter, req *http.Request) {
-	templ.Handler(templates.CreatePage(p.template)).ServeHTTP(res, req)
+	user, _ := models.UserFromCtx(req.Context())
+	templ.Handler(templates.CreatePage(templates.InternalError(user, p.err.UserMessage))).ServeHTTP(res, req)
 }
 
-// PartialResponse renders the error message in the content area.
+// PartialResponse will render the error in the content area for GET requests, as a notification otherwise.
 func (p *InternalError) PartialResponse(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set(htmx.HeaderRetarget, templates.ContentID.Target())
-	templ.Handler(p.template, templ.WithFragments(templates.ErrorFragment)).ServeHTTP(res, req)
+	if req.Method == http.MethodGet {
+		user, _ := models.UserFromCtx(req.Context())
+		res.Header().Set(htmx.HeaderRetarget, templates.ContentID.Target())
+		templ.Handler(templates.InternalError(user, p.err.UserMessage), templ.WithFragments(templates.ErrorFragment)).
+			ServeHTTP(res, req)
+		return
+	} else {
+		res.Header().Set(htmx.HeaderReswap, "none")
+		templ.Handler(templates.Notification(p.err.UserMessage, 0)).ServeHTTP(res, req)
+	}
 }
 
 // ExternalError represents errors shown on external (public) pages.
-type ExternalError Error
+type ExternalError struct {
+	template templ.Component
+}
 
 // HandleExternalError handles display errors on external pages.
 func HandleExternalError(err error) http.HandlerFunc {
@@ -68,7 +84,7 @@ func HandleExternalError(err error) http.HandlerFunc {
 			user, _ := models.UserFromCtx(req.Context())
 			apiErr.WriteLog(req.Context())
 			res.WriteHeader(apiErr.HTTPStatus())
-			page := &InternalError{
+			page := &ExternalError{
 				template: templates.ExternalError(user, apiErr.GetUserMessage()),
 			}
 			RenderPage(page).ServeHTTP(res, req)
@@ -89,5 +105,5 @@ func (p *ExternalError) FullResponse(res http.ResponseWriter, req *http.Request)
 
 // PartialResponse renders the error message itself.
 func (p *ExternalError) PartialResponse(res http.ResponseWriter, req *http.Request) {
-	templ.Handler(p.template, templ.WithFragments(templates.ErrorFragment)).ServeHTTP(res, req)
+	res.WriteHeader(http.StatusNoContent)
 }
