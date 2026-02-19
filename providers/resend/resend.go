6ff70aebc1df3c48713e5536a4796d23e1c30a38
Joshua Rich <joshua.rich@gmail.com>
2026-02-19 23:25:08 +0000

    refactor(resend): :recycle: restructure code

--- providers/resend/resend.go
+++ providers/resend/resend.go
@@ -9,14 +9,47 @@ import (
 	"sync"
 
 	"github.com/resend/resend-go/v3"
+
+	"github.com/immanent-tech/foragd/config"
+	"github.com/immanent-tech/foragd/validation"
+)
+
+const (
+	// ConfigEnvPrefix is the prefix applied to environment variables for configuring Resend.
+	ConfigEnvPrefix = config.ConfigEnvPrefix + "RESEND_"
 )
 
 var ErrInvalidEmail = errors.New("email is invalid")
 
+var cfg Config
+
+// Config structure.
+type Config struct {
+	WebHookSecret string `koanf:"webhooksecret" validate:"required"`
+	APIKey        string `koanf:"apikey"        validate:"required"`
+}
+
+// loadClient loads the resend API client and ensures this is only done one time, no matter how many times it is called.
 var loadClient = sync.OnceValues(func() (*resend.Client, error) {
 	if err := loadConfig(); err != nil {
 		return nil, fmt.Errorf("load config: %w", err)
 	}
 	client := resend.NewClient(cfg.APIKey)
 	return client, nil
+})
+
+// loadConfig loads the Resend configuration and ensures this is only done one time, no matter how many times it is
+// called.
+var loadConfig = sync.OnceValue(func() error {
+	var err error
+	cfg, err = config.Load[Config](ConfigEnvPrefix)
+	if err != nil {
+		return fmt.Errorf("load environment variables: %w", err)
+	}
+
+	err = validation.Validate.Struct(cfg)
+	if err != nil {
+		return fmt.Errorf("validate config: %w", err)
+	}
+	return nil
 })
