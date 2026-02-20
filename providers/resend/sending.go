b1a926b6ddba30e224b151b2a94724dfb286b88d
Joshua Rich <joshua.rich@gmail.com>
2026-02-20 03:54:23 +0000

    refactor(resend): :recycle: abstract sending email so that it can be done with both templates and regular emails

--- providers/resend/sending.go
+++ providers/resend/sending.go
@@ -14,36 +14,62 @@ import (
 	slogctx "github.com/veqryn/slog-context"
 )
 
-type templateEmail struct {
-	*resend.EmailTemplate
-	tags []resend.Tag
+type email struct {
+	*resend.Email
+
+	template *resend.EmailTemplate
+	tags     []resend.Tag
 
 	mu sync.Mutex
 }
 
-type TemplateEmailOption func(*templateEmail)
+// EmailOption is a functional option to apply to an email.
+type EmailOption func(*email)
 
-func WithTemplateVariable(key string, value any) TemplateEmailOption {
-	return func(te *templateEmail) {
-		te.mu.Lock()
-		defer te.mu.Unlock()
-		te.Variables[key] = value
+// To option sets the to address of an email.
+func To(to ...string) EmailOption {
+	return func(e *email) {
+		e.To = append(e.To, to...)
 	}
 }
 
-func WithTag(key string, value string) TemplateEmailOption {
-	return func(te *templateEmail) {
-		te.mu.Lock()
-		defer te.mu.Unlock()
-		te.tags = append(te.tags, resend.Tag{Name: key, Value: value})
+// Cc option sets the cc address of an email.
+func Cc(cc ...string) EmailOption {
+	return func(e *email) {
+		e.Cc = append(e.Cc, cc...)
+	}
+}
+
+// Bcc option sets the bcc address of an email.
+func Bcc(bcc ...string) EmailOption {
+	return func(e *email) {
+		e.Bcc = append(e.Bcc, bcc...)
+	}
+}
+
+// WithTemplateVariable option assigns a value to the given template variable in the email template.
+func WithTemplateVariable(key string, value any) EmailOption {
+	return func(e *email) {
+		e.mu.Lock()
+		defer e.mu.Unlock()
+		e.template.Variables[key] = value
+	}
+}
+
+// WithTag option applies the given tag to the email.
+func WithTag(key string, value string) EmailOption {
+	return func(e *email) {
+		e.mu.Lock()
+		defer e.mu.Unlock()
+		e.tags = append(e.tags, resend.Tag{Name: key, Value: value})
 	}
 }
 
 // SendTemplatedEmail sends the template with the given id to the given address, with any additional template options
 // applied.
-func SendTemplatedEmail(ctx context.Context, to string, templateID string, options ...TemplateEmailOption) error {
-	template := &templateEmail{
-		EmailTemplate: &resend.EmailTemplate{
+func SendTemplatedEmail(ctx context.Context, templateID string, options ...EmailOption) error {
+	template := &email{
+		template: &resend.EmailTemplate{
 			Id:        templateID,
 			Variables: make(map[string]interface{}),
 		},
@@ -53,25 +79,37 @@ func SendTemplatedEmail(ctx context.Context, to string, templateID string, optio
 		option(template)
 	}
 
+	// Check validity.
+	switch {
+	case len(template.To) == 0 || len(template.Bcc) == 0 || len(template.Cc) == 0:
+		// Must have a To, Cc, or Bcc.
+		return fmt.Errorf("%w: no address(es) specified", ErrInvalidEmail)
+	case template.template.Id == "":
+		// Must specify a template ID.
+		return fmt.Errorf("%w: no template specified", ErrInvalidEmail)
+	}
+
 	client, err := loadClient()
 	if err != nil {
 		return fmt.Errorf("load client: %w", err)
 	}
 
 	params := &resend.SendEmailRequest{
-		To:       []string{to},
-		Template: template.EmailTemplate,
+		To:       template.To,
+		Cc:       template.Cc,
+		Bcc:      template.Bcc,
+		Template: template.template,
 		Tags:     template.tags,
 	}
-
-	slogctx.FromCtx(ctx).Debug("Sending templated email.",
-		slog.String("template", templateID),
-	)
 
 	_, err = client.Emails.Send(params)
 	if err != nil {
 		return fmt.Errorf("send template email: %w", err)
 	}
+
+	slogctx.FromCtx(ctx).Debug("Templated email sent.",
+		slog.String("template", templateID),
+	)
 
 	return nil
 }
