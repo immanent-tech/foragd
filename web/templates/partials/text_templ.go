3dbea5b53507d96ee896bd8d1b6521881dd8637f
Joshua Rich <joshua.rich@gmail.com>
2026-03-02 06:10:41 +0000

    refactor(templates): :recycle: add scroll margin top on headings

--- web/templates/partials/text_templ.go
+++ web/templates/partials/text_templ.go
@@ -33,7 +33,7 @@ func Heading1(text string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<h1 class=\"mt-4 mb-2 text-5xl font-semibold tracking-tight text-balance capitalize sm:mt-8 sm:mb-4 sm:text-7xl\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<h1 class=\"mt-4 mb-2 text-5xl font-semibold tracking-tight text-balance capitalize sm:mt-8 sm:mb-4 sm:text-7xl scroll-mt-2\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -79,7 +79,7 @@ func Heading2() templ.Component {
 			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<h2 class=\"mt-4 mb-2 text-2xl/7 font-bold tracking-tight capitalize sm:mt-8 sm:mb-4 sm:text-3xl\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<h2 class=\"mt-4 mb-2 text-2xl/7 font-bold tracking-tight capitalize sm:mt-8 sm:mb-4 sm:text-3xl scroll-mt-1.5\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -116,7 +116,7 @@ func Heading3() templ.Component {
 			templ_7745c5c3_Var4 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "<h3 class=\"mt-4 mb-2 text-lg font-medium capitalize sm:mt-8 sm:mb-4\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "<h3 class=\"mt-4 mb-2 text-lg font-medium capitalize sm:mt-8 sm:mb-4 scroll-mt-1.5\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
