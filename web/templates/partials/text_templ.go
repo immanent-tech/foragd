2b6faa7a6f3fc6da2ae623033c7496fe214c1796
Joshua Rich <joshua.rich@gmail.com>
2026-03-08 23:38:46 +0000

    refactor(templates/partials): :recycle: don't capitalize headings by default

--- web/templates/partials/text_templ.go
+++ web/templates/partials/text_templ.go
@@ -33,7 +33,7 @@ func Heading1() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<h1 class=\"mt-4 mb-2 text-2xl/7 font-semibold tracking-tight text-balance capitalize sm:mt-8 sm:mb-4 sm:text-3xl scroll-mt-16\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<h1 class=\"mt-4 mb-2 text-2xl/7 font-semibold tracking-tight text-balance sm:mt-8 sm:mb-4 sm:text-3xl scroll-mt-16\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -70,7 +70,7 @@ func Heading2() templ.Component {
 			templ_7745c5c3_Var2 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<h2 class=\"mt-4 mb-2 text-2xl/7 font-bold tracking-tight capitalize sm:mt-8 sm:mb-4 sm:text-3xl scroll-mt-16\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<h2 class=\"mt-4 mb-2 text-2xl/7 font-bold tracking-tight sm:mt-8 sm:mb-4 sm:text-3xl scroll-mt-16\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -107,7 +107,7 @@ func Heading3() templ.Component {
 			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "<h3 class=\"mt-4 mb-2 text-lg font-medium capitalize sm:mt-8 sm:mb-4 scroll-mt-1.5\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "<h3 class=\"mt-4 mb-2 text-lg font-medium sm:mt-8 sm:mb-4 scroll-mt-1.5\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
