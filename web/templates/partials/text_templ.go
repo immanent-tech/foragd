86c4a4114545d7c42c3ebfe94e36c3577aa65b9a
Joshua Rich <joshua.rich@gmail.com>
2026-03-12 00:04:56 +0000

    fix(templates/partials): :lipstick: improved styling of headers
    - make h1 stand out over h2

--- web/templates/partials/text_templ.go
+++ web/templates/partials/text_templ.go
@@ -33,7 +33,7 @@ func Heading1() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<h1 class=\"mt-4 mb-2 text-2xl/7 font-semibold tracking-tight text-balance sm:mt-8 sm:mb-4 sm:text-3xl scroll-mt-16\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<h1 class=\"mt-4 mb-2 text-2xl/7 font-bold tracking-tight text-balance sm:mt-8 sm:mb-4 sm:text-3xl scroll-mt-16\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -70,7 +70,7 @@ func Heading2() templ.Component {
 			templ_7745c5c3_Var2 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<h2 class=\"mt-4 mb-2 text-2xl/7 font-bold tracking-tight sm:mt-8 sm:mb-4 sm:text-3xl scroll-mt-16\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<h2 class=\"mt-4 mb-2 text-xl/7 font-semibold tracking-tight sm:mt-8 sm:mb-4 sm:text-2xl scroll-mt-16\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
