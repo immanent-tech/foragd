591b5a7dd6ec950aa1f45f973b0eb2a0b74af6de
Joshua Rich <joshua.rich@gmail.com>
2026-03-27 02:50:22 +0000

    feat(email): :sparkles: add unsubscribe link to new inactive user email

--- scheduler/jobs/jobs_users.go
+++ scheduler/jobs/jobs_users.go
@@ -72,6 +72,11 @@ func (j *newInactiveUserJob) Execute(ctx context.Context) error {
 		return fmt.Errorf("get user: %w", err)
 	}
 
+	unsubscribeToken, err := resend.EncodeEmail(user.GetEmail())
+	if err != nil {
+		return fmt.Errorf("generate unsubscribe token: %w", err)
+	}
+
 	// If the user has not yet logged in.
 	if user.LoginCount <= 1 {
 		// Create and send email to user.
@@ -80,6 +85,7 @@ func (j *newInactiveUserJob) Execute(ctx context.Context) error {
 			resend.To(user.GetEmail()),
 			resend.WithTag(resend.TagCategory, resend.TagCategoryPromotional),
 			resend.WithVariable("USER_NICKNAME", user.GetNickname()),
+			resend.WithVariable("USER_UNSUBSCRIBE_LINK", "https://foragd.app/unsubscribe/"+unsubscribeToken),
 		)
 		if err != nil {
 			return fmt.Errorf("create new inactive user email: %w", err)
