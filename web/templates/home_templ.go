464b7addb93361b48d2eb2d55ac8e43d2e487cd1
Joshua Rich <joshua.rich@gmail.com>
2026-04-12 02:09:43 +0000

    fix(templates/home): :lipstick: align cards in carousel in center

--- web/templates/home_templ.go
+++ web/templates/home_templ.go
@@ -101,7 +101,7 @@ func UserHome(data *models.HomeResponse) templ.Component {
 								return templ_7745c5c3_Err
 							}
 						}
-						templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</div><div class=\"carousel w-full carousel-center space-x-4 p-4 content-auto sm:hidden\">")
+						templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</div><div class=\"carousel w-full carousel-center space-x-4 p-4 content-auto sm:hidden items-center\">")
 						if templ_7745c5c3_Err != nil {
 							return templ_7745c5c3_Err
 						}
