3ababa318370e6179c1d95666191796c07c66893
Joshua Rich <joshua.rich@gmail.com>
2026-05-01 01:52:21 +0000

    chore: :construction: perform one-shot notification to inactive accounts about pending deletion

---
+++ oneshots/20260501/README.md
@@ -0,0 +1,11 @@
+<!--
+ Copyright 2026 Joshua Rich <joshua.rich@gmail.com>.
+ SPDX-License-Identifier: 	AGPL-3.0-or-later
+-->
+
+# Notify Inactive Accounts of Deletion
+
+Send emails to accounts that have not logged in for at least a month and/or have few login counts. i.e., inactive
+accounts.
+
+Accounts have been marked for deletion in 7 days.
---
+++ oneshots/20260501/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"slices"
+	"time"
+
+	slogctx "github.com/veqryn/slog-context"
+
+	"github.com/immanent-tech/foragd/models"
+	"github.com/immanent-tech/foragd/models/schema"
+	"github.com/immanent-tech/foragd/providers/elastic"
+	"github.com/immanent-tech/foragd/providers/elastic/query"
+	"github.com/immanent-tech/foragd/providers/resend"
+)
+
+func main() {
+	ctx := context.TODO()
+
+	if err := elastic.Connect(); err != nil {
+		panic(err)
+	}
+
+	resp, err := elastic.Search[*models.User](
+		ctx,
+		schema.UsersIndexRO(),
+		query.Bool(
+			query.Filter(
+				// Account should be older than 30 days.
+				query.Before("created_at", time.Now().UTC().Add(-30*24*time.Hour)),
+				query.Bool(
+					// Match any of these.
+					query.Should(
+						// Account was created but has never logged in.
+						query.NumberRange("login_count", query.IntLessThan(2)),
+						query.Bool(
+							query.MustNot(
+								query.Exists("last_login"),
+							),
+						),
+						// Last login is more than 30 days ago.
+						query.Before("last_login", time.Now().UTC().Add(-30*24*time.Hour)),
+					),
+				),
+			),
+			query.MustNot(
+				// Should not already be pending deletion.
+				query.Term("metadata.pending_deletion", true),
+			),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	for user := range slices.Values(resp.Results) {
+		// Create and send email to user.
+		email, err := resend.NewTemplatedEmail(
+			"inactive-account-deletion-notice",
+			resend.To(user.GetEmail()),
+			resend.WithTag(resend.TagCategory, resend.TagCategoryAccount),
+		)
+		if err != nil {
+			panic(err)
+		}
+		if err := resend.SendEmail(ctx, resend.WithExistingEmail(email)); err != nil {
+			panic(err)
+		}
+		metadata := user.Metadata
+		metadata.PendingDeletion = new(true)
+		if err := models.UpdateUser(ctx, user.GetID(), map[string]any{
+			"metadata": metadata,
+		}); err != nil {
+			panic(err)
+		}
+		slogctx.FromCtx(ctx).Info("Sent email and marked account for deletion.",
+			slog.String("user_id", user.GetID()),
+			slog.String("user_email", user.GetEmail()),
+		)
+	}
+}
