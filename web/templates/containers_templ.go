24a0cb5853c3c0e0545745a4b45e0570f0426312
Joshua Rich <joshua.rich@gmail.com>
2026-05-04 05:29:23 +0000

    refactor(templates): :recycle: code clean-up

--- web/templates/containers_templ.go
+++ web/templates/containers_templ.go
@@ -147,7 +147,7 @@ func content(options ...element.PropertiesOption) templ.Component {
 		var templ_7745c5c3_Var6 string
 		templ_7745c5c3_Var6, templ_7745c5c3_Err = templ.JoinStringErrs(ContentID.String())
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates/containers.templ`, Line: 47, Col: 25}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates/containers.templ`, Line: 38, Col: 25}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var6))
 		if templ_7745c5c3_Err != nil {
