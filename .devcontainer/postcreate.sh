f04597567c84841b232d2b840adc907fd6feb2be
Joshua Rich <joshua.rich@gmail.com>
2026-05-13 14:24:25 +1000

    build(devcontainer): :recycle: switch over to an ubuntu based devcontainer for bubblewrap

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -15,6 +15,7 @@ export PATH="$HOME/go/bin:/go/bin:/usr/local/go/bin:$PATH" && \
     go install golang.org/x/tools/gopls@latest && \
     go install github.com/air-verse/air@latest && \
     go install github.com/a-h/templ/cmd/templ@latest && \
+    go install github.com/sigstore/cosign/v3/cmd/cosign@latest && \
     curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.8.0 && \
     golangci-lint custom && \
     mv /tmp/golangci-lint-v2 $(go env GOPATH)/bin/
@@ -30,7 +31,6 @@ cd $HOME && \
     curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz && \
     tar -xf google-cloud-cli-linux-x86_64.tar.gz && \
     rm google-cloud-cli-linux-x86_64.tar.gz && \
-    sudo apk add python3 && \
     google-cloud-sdk/install.sh --usage-reporting false --quiet --additional-components app-engine-go && \
     echo 'source /home/vscode/google-cloud-sdk/path.fish.inc' >> ~/.config/fish/config.fish
 
