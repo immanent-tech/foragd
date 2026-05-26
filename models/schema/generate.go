e87a9c0e0b84e6dbffdbc493a6534fbc2f1077f8
Joshua Rich <joshua.rich@gmail.com>
2026-05-26 15:50:39 +1000

    refactor(jobs): ♻️ move openapi code for jobs to jobs package

--- models/schema/generate.go
+++ models/schema/generate.go
@@ -5,4 +5,3 @@ package schema
 
 //go:generate go tool oapi-codegen -config models-cfg.yaml models.yaml
 //go:generate go tool oapi-codegen -config api-cfg.yaml api.yaml
-//go:generate go tool oapi-codegen -config jobs-cfg.yaml jobs.yaml
--- scheduler/jobs/jobs.go
+++ scheduler/jobs/jobs.go
@@ -1,6 +1,7 @@
 // Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
 // SPDX-License-Identifier: 	AGPL-3.0-or-later
 
+//go:generate go tool oapi-codegen -config jobs-cfg.yaml jobs.yaml
 package jobs
 
 import (
