653cc1e622d6a17ebb9a3fd25f892fbc39fff3ff
Joshua Rich <joshua.rich@gmail.com>
2026-05-29 15:42:21 +1000

    perf(server): ⚡ add instrumentation to user methods

--- service/users.go
+++ service/users.go
@@ -19,6 +19,7 @@ import (
 	"github.com/immanent-tech/foragd/providers/auth0"
 	"github.com/immanent-tech/foragd/providers/elastic"
 	"github.com/immanent-tech/foragd/providers/elastic/query"
+	"github.com/immanent-tech/foragd/server/otel"
 )
 
 var userCache = otter.Must(&otter.Options[string, models.User]{
@@ -46,6 +47,10 @@ func loadUser(ctx context.Context, id string) (models.User, error) {
 
 // GetUser retrieves the user doc with the given id.
 func GetUser(ctx context.Context, id models.UserID) (*models.User, error) {
+	_, span := otel.TracerProvider.Tracer("").
+		Start(ctx, "get-user")
+	defer span.End()
+
 	user, err := elastic.GetDoc[models.UserID, *models.User](ctx, schema.UsersIndexRO(), id)
 	if err != nil || user == nil {
 		return nil, fmt.Errorf("get user: %w", err)
@@ -55,6 +60,10 @@ func GetUser(ctx context.Context, id models.UserID) (*models.User, error) {
 
 // GetUserByExternalID will search for and return a user that matches the given external ID, if exists.
 func GetUserByExternalID(ctx context.Context, externalID string) (*models.User, error) {
+	_, span := otel.TracerProvider.Tracer("").
+		Start(ctx, "get-user-by-external-id")
+	defer span.End()
+
 	switch user, err := userCache.Get(ctx, externalID, otter.LoaderFunc[string, models.User](loadUser)); {
 	case err != nil && !errors.Is(err, elastic.ErrNotFound):
 		return nil, fmt.Errorf("find user by external id: %w", err)
@@ -124,6 +133,10 @@ func GetUserBySubscriptionID(ctx context.Context, id string) (*models.User, erro
 
 // UpdateUser will apply the given updates to the user.
 func UpdateUser(ctx context.Context, user *models.User, updates map[string]any) error {
+	_, span := otel.TracerProvider.Tracer("").
+		Start(ctx, "update-user")
+	defer span.End()
+
 	updates["updated_at"] = time.Now().UTC()
 	if err := elastic.UpdateDoc(ctx, schema.UsersIndexRW(), user.GetID(), updates,
 		elastic.WithRefresh(true),
@@ -139,6 +152,10 @@ func UpdateUser(ctx context.Context, user *models.User, updates map[string]any) 
 
 // SyncUser tries to sync relevant user data from the auth backend to the local data.
 func SyncUser(ctx context.Context, localUser *models.User) {
+	_, span := otel.TracerProvider.Tracer("").
+		Start(ctx, "sync-user")
+	defer span.End()
+
 	auth0User, err := auth0.GetUser(ctx, localUser.GetExternalID())
 	if err != nil {
 		slogctx.FromCtx(ctx).Error("Could not sync user data.",
