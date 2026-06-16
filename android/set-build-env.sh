0bbe1dbb0ca5864f63d6386ee6fcb7af4d694266
Joshua Rich <joshua.rich@gmail.com>
2026-06-16 10:57:11 +1000

    build(android): ♻️ support building developement and production versions of apk
    - allow customisation of base url in twa-manifest.json
    - adjust github android build pipeline

---
+++ android/set-build-env.sh
@@ -0,0 +1,11 @@
+# Copyright 2026 Joshua Rich <joshua.rich@gmail.com>.
+# SPDX-License-Identifier: 	AGPL-3.0-or-later
+
+#!/usr/bin/env bash
+
+sed \
+  -e "s|__ANDROID_KEYSTORE_PATH__|${ANDROID_KEYSTORE_PATH}|g" \
+  -e "s|__ANDROID_KEYSTORE_ALIAS__|${ANDROID_KEYSTORE_ALIAS}|g" \
+  -e "s|__ANDROID_KEYSTORE_FINGERPRINT__|${ANDROID_KEYSTORE_FINGERPRINT}|g" \
+  -e "s|__FORAGD_BASEURL__|${FORAGD_BASEURL}|g" \
+  twa-manifest.json > twa-manifest-generated.json
