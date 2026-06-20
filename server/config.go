0b4c5f7e8912037f831d3ac3abcbefe07518b020
Joshua Rich <joshua.rich@gmail.com>
2026-06-20 15:15:08 +1000

    perf(server): ⚡ don't compress woff2

--- server/config.go
+++ server/config.go
@@ -23,7 +23,7 @@ var cfg = Config{
 	Port:                 7000,
 	Host:                 "0.0.0.0",
 	CompressionLevel:     5,
-	CompressionMimetypes: []string{"text/html", "text/css", "text/javascript", "font/woff2", "image/svg+xml"},
+	CompressionMimetypes: []string{"text/html", "text/css", "text/javascript", "image/svg+xml"},
 	ReadTimeout:          "30s",
 	WriteTimeout:         "120s",
 	IdleTimeout:          "900s",
