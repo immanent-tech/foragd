3b5e50bddba2fe1d33233cf9f6c9547305f33394
Joshua Rich <joshua.rich@gmail.com>
2026-06-22 09:47:05 +1000

    fix(android): 🔧 twa fixes
    - support changing twa domain (for development builds)
    - fix utm tag

--- android/set-build-env.sh
+++ android/set-build-env.sh
@@ -7,5 +7,6 @@ sed \
   -e "s|__ANDROID_KEYSTORE_PATH__|${ANDROID_KEYSTORE_PATH}|g" \
   -e "s|__ANDROID_KEYSTORE_ALIAS__|${ANDROID_KEYSTORE_ALIAS}|g" \
   -e "s|__ANDROID_KEYSTORE_FINGERPRINT__|${ANDROID_KEYSTORE_FINGERPRINT}|g" \
+  -e "s|__FORAGD_DOMAIN__|${FORAGD_DOMAIN}|g" \
   -e "s|__FORAGD_BASEURL__|${FORAGD_BASEURL}|g" \
   twa-manifest.json > twa-manifest-generated.json
