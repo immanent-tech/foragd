efccf773059f1938db5965ecd463e6580fb22599
Joshua Rich <joshua.rich@gmail.com>
2026-06-26 15:16:16 +1000

    feat(service/items): ✨ collapse on title to remove potential duplicate items from results

--- service/items.go
+++ service/items.go
@@ -93,6 +93,7 @@ func SearchItems(
 		elastic.WithSort(NewItemSortOptions(sort)...),
 		elastic.WithSearchAfter(searchAfter...),
 		elastic.WithSize(count),
+		elastic.WithCollapseField("title.raw"),
 	)
 	if err != nil {
 		return nil, "", fmt.Errorf("search items: %w", err)
