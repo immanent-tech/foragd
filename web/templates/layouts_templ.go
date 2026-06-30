41fa2b8c5cf36e0f9e08dc175501fb2adc37f5b0
Joshua Rich <joshua.rich@gmail.com>
2026-06-30 10:03:00 +1000

    fix(templates/layouts): 🐛 fix scroll on external pages

--- web/templates/layouts_templ.go
+++ web/templates/layouts_templ.go
@@ -162,7 +162,7 @@ func LayoutExternal(content ...templ.Component) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, "\" class=\"w-screen flex-1 scroll-mt-16 pt-16 px-safe pb-20 overflow-y-scroll\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, "\" class=\"w-screen flex-1 scroll-mt-16 pt-16 px-safe pb-20\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
