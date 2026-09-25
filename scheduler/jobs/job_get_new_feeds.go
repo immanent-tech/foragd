8908dfaf92f797e2b4186b8714c607d51954be47
Joshua Rich <joshua.rich@gmail.com>
2026-09-25 02:04:42 +0000

    perf(scheduler/jobs): ⚡ improved get new feeds job
    - throttle concurrent new feed processing to avoid stampeding new feed updates
    - smarter logic for checking for existing feed job

--- scheduler/jobs/job_get_new_feeds.go
+++ scheduler/jobs/job_get_new_feeds.go
@@ -10,6 +10,7 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
+	"runtime"
 	"slices"
 	"sync"
 	"time"
@@ -73,20 +74,50 @@ func ExecuteGetNewFeeds(ctx context.Context, job *SerializedJob) error {
 		)
 	}
 
-	var wg sync.WaitGroup
-	// Create new feed jobs where necessary.
-	for feed := range slices.Values(newFeeds) {
-		// Add additional feed details to logs.
-		feedCtx := slogctx.With(ctx, "feed_id", feed.GetID())
-		feedCtx = slogctx.With(feedCtx, "feed_name", feed.GetTitle())
-		wg.Go(func() {
-			if err := AddFeedJob(feedCtx, schedulerAPI, feedSvc, feed); err != nil {
-				slogctx.Error(ctx, "Unable to add feed job",
-					slog.Any("error", err),
-				)
-			}
-		})
+	// Get job keys for existing feed jobs.
+	existingJobKeys, err := schedulerAPI.GetJobKeys(matcher.JobGroupEquals("update_feed"))
+	if err != nil {
+		return fmt.Errorf("get job keys: %w", err)
 	}
+
+	maxConcurrentImports := runtime.GOMAXPROCS(0)
+	feedCh := make(chan *models.Feed, maxConcurrentImports*5)
+	var wg sync.WaitGroup
+
+	for range maxConcurrentImports {
+		// Create new feed jobs where necessary.
+		for feed := range slices.Values(newFeeds) {
+			// Add additional feed details to logs.
+			feedCtx := slogctx.With(ctx, "feed_id", feed.GetID())
+			feedCtx = slogctx.With(feedCtx, "feed_name", feed.GetTitle())
+			// Skip adding if there is already an existing job.
+			if slices.ContainsFunc(existingJobKeys, func(e *quartz.JobKey) bool {
+				return e.Name() == feed.GetID()
+			}) {
+				continue
+			}
+			// Add and process update feed job.
+			wg.Go(func() {
+				if err := AddFeedJob(feedCtx, schedulerAPI, feedSvc, feed); err != nil {
+					slogctx.Error(ctx, "Unable to add feed job",
+						slog.Any("error", err),
+					)
+				}
+			})
+		}
+	}
+
+	for feed := range slices.Values(newFeeds) {
+		select {
+		case feedCh <- feed:
+		case <-ctx.Done():
+			close(feedCh)
+			wg.Wait()
+			return ctx.Err()
+		}
+
+	}
+	close(feedCh)
 
 	wg.Wait()
 
@@ -96,21 +127,16 @@ func ExecuteGetNewFeeds(ctx context.Context, job *SerializedJob) error {
 	return nil
 }
 
-func AddFeedJob(ctx context.Context, scheduler SchedulerAPI, feedSvc *service.FeedService, feed *models.Feed) error {
+func AddFeedJob(
+	ctx context.Context,
+	scheduler SchedulerAPI,
+	feedSvc *service.FeedService,
+	feed *models.Feed,
+) error {
 	// Create update feed job.
 	job, err := NewUpdateFeedJob(ctx, feedSvc, feed.GetID())
 	if err != nil {
 		return fmt.Errorf("new update feed job: %w", err)
-	}
-
-	// Check for an existing job.
-	keys, err := scheduler.GetJobKeys(matcher.JobNameEquals(job.JobDetail().JobKey().Name()))
-	if err != nil {
-		return fmt.Errorf("get job keys: %w", err)
-	}
-	if len(keys) > 0 {
-		slogctx.Warn(ctx, "Existing job found, ignoring.")
-		return nil
 	}
 
 	// Schedule the new job.
